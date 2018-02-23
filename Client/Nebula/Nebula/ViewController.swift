b7330d70525adcb4317c5cacbf7aecc023efda7f
Jordan Campbell <jordancampbell@Leonies-MacBook-Pro.local>
2018-02-23 15:13:13 +1300

    clean nebula/Client 01

--- Client/Nebula/Nebula/ViewController.swift
+++ Client/Nebula/Nebula/ViewController.swift
@@ -31,7 +31,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     var frameCounter: Int = 0
     
     var atlasSession: ARSession = ARSession()
-//    var nebula: Nebula = Nebula()
     
     var counter: Int = 0
     
@@ -51,12 +50,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         // Set the scene to the view
         sceneView.scene = scene
         
-        //        atlasSession = ARSession()
-        let cube = SCNBox(width: CGFloat(0.1), height: CGFloat(0.5), length: CGFloat(0.1), chamferRadius: CGFloat(0.001))
-        cube.firstMaterial?.diffuse.contents = UIColor.magenta.withAlphaComponent(CGFloat(0.5))
-        let node = SCNNode(geometry: cube)
-        node.position = SCNVector3Make(0.05,0.25,0.05)
-        self.sceneView.scene.rootNode.addChildNode(node)
         self.addButton()
     }
     
@@ -92,26 +85,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         // we only want to write data when we have turned tracking off, and when there are no frames left to be written
         if self.frameCounter == 0 && self.trackingON == false {
             
-            // extract the position and rotation of the camera
-            
-//            var data: [String : [String: Float]] = [String: [String: Float]]()
-//            DispatchQueue.global(qos: .utility).async {
-//                for (key, _value) in self.jsonObject {
-//                    if let value = _value as? [String: Any] {
-//                        let temp = [:]
-//                        if let _currentPosition = value["cameraPos"] as? [String: Float],
-//                           let _currentRotation = value["cameraEulerAngle"] as? [String: Float] {
-////                            temp["position"]
-//                        }
-//                    }
-//                }
-            
-//                data[key]["rotation"] = _currentRotation
-//                data[key]["position"] = _currentPosition
-                
-//                print(data)
-//            }
-            
             DispatchQueue.global(qos: .utility).async {
                 
                 print("Writing data:", self.frameCounter)
@@ -120,10 +93,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
                 if valid {
                     let json = JSON(self.jsonObject)
                     let representation = json.rawString([.castNilToNSNull: true])
-                    
-//                    if let data = representation?.description {
-//                        self.nebula.sendData(data)
-//                    }
                     
                     let jsonFilePath = getFilePath(fileFolder: self.recordStartTime!, fileName: getCurrentTime()+".json")
                     do {
@@ -145,8 +114,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
         
-        // check whether we can track anything
-        //        if self.track
         if let state = self.sceneView.session.currentFrame?.camera.trackingState {
             switch(state) {
             case .normal:
@@ -182,20 +149,8 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
                     
                     let json = JSON(jsonNode)
                     let representation = json.rawString([.castNilToNSNull: true])
-//                    if let data = representation?.description {
-//                        self.nebula.sendData(data)
-//                    }
                     
                     let image = UIImageJPEGRepresentation(pixelBufferToUIImage(pixelBuffer: frame.capturedImage), 0.25)!
-                    
-//                    var imageData: [String: String] = [:]
-//                    imageData["imagename"] = jsonNode["imagename"] as! String
-//                    imageData["image"] = image.base64EncodedString(options: .lineLength64Characters)
-//
-//                    print(imageData["image"])
-//                    print("Sending: \(self.counter) images")
-//                    self.counter += 1
-//                    self.nebula.sendImage( imageData )
                     
                     let filePath = getFilePath(fileFolder: self.recordStartTime!, fileName: jsonNode["imagename"] as! String)
                     try? image.write(to: URL(fileURLWithPath: filePath))
@@ -222,33 +177,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         
         // Run the view's session
         sceneView.session.run(configuration)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
 
