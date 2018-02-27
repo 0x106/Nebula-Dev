21a50fdf1e818c01a7a6afcf0eda6462c88b5b49
Jordan Campbell <jordan.evan.campbell@gmail.com>
2018-02-28 10:48:41 +1300

    fixed login problems

--- Client/Nebula/Nebula/Login.swift
+++ Client/Nebula/Nebula/Login.swift
@@ -22,33 +22,63 @@ class LoginController: UIViewController, GIDSignInUIDelegate {
         self.metadata = initMetadata()
         
         GIDSignIn.sharedInstance().uiDelegate = self
-        GIDSignIn.sharedInstance().signIn()
+//        GIDSignIn.sharedInstance().signIn()
         
         handle = Auth.auth().addStateDidChangeListener { (auth, user) in
             
-            if var _ = self.metadata {
-                if self.metadata!["metauser"].stringValue == "" {
-                    let metauser: JSON = [
-                        "uid": user?.uid ?? "unknown"
-                    ]
-                    self.metadata!["metauser"] = metauser
-                    updateMetadata(self.metadata!)
-                } else {
-                    if self.metadata!["metauser"].stringValue != user?.uid {
-                        fatalError("User ID doesn't match recorded value.")
+            print(user)
+            
+            if user != nil {
+            
+                if var _ = self.metadata {
+                    if self.metadata!["metauser"].stringValue == "" {
+                        let metauser: JSON = [
+                            "uid": user?.uid ?? "unknown"
+                        ]
+                        self.metadata!["metauser"] = metauser
+                        updateMetadata(self.metadata!)
+                    } else {
+                        if self.metadata!["metauser"].stringValue != user?.uid {
+                            fatalError("User ID doesn't match recorded value.")
+                        }
                     }
                 }
-            }
             
-            DispatchQueue.main.async {
-                self.performSegue(withIdentifier: "login_tableViewSegue", sender: self)
+                DispatchQueue.main.async {
+                    self.performSegue(withIdentifier: "login_tableViewSegue", sender: self)
+                }
             }
         }
     }
+    
     
     override func viewWillDisappear(_ animated: Bool) {
         Auth.auth().removeStateDidChangeListener(handle!)
     }
     
+}
+
+class LogoutController: UIViewController {
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        print("Loading logout controller")
+//    }
+    
+    @IBAction func logout(_ sender: Any) {
+        print("logout")
+        if let _ = Auth.auth().currentUser {
+            do {
+                try? Auth.auth().signOut()
+                if let user = Auth.auth().currentUser {
+                    
+                } else {
+                    print("logged out")
+                }
+//                let loginVC = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+            }
+        }
+    }
+    
+
 }
 
